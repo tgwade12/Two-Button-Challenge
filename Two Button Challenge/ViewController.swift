e56eee10fc17fe83ee55eb9a0fbd46fbe0d79161
Thomas Wade <wadetc@bc.edu>
2020-01-20 22:52:13 -0500

    Initial Commit

---
+++ Two Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Button Challenge
+//
+//  Created by Thomas Wade on 1/20/20.
+//  Copyright © 2020 Thomas Wade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
